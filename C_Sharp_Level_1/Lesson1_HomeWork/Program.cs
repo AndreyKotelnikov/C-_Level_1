5b4b9b153f5f2fc032c91ae52f43d175fdc131d2
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-14 21:06:26 +0300

    2.	Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.

--- C_Sharp_Level_1/Lesson1_HomeWork/Program.cs
+++ C_Sharp_Level_1/Lesson1_HomeWork/Program.cs
@@ -31,6 +31,11 @@ namespace Lesson1_HomeWork
             }
         }
 
+        static double BodyMassIndex(double growth, double weight)
+        {
+            return weight / Math.Pow(growth, 2);
+        }
+
         static void Main(string[] args)
         {
             /*1.	Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). 
@@ -49,7 +54,13 @@ namespace Lesson1_HomeWork
             Console.WriteLine("{0} {1}, возраст: {2}, рост: {3:N2}, вес: {4:N2}", name, surname, age, growth, weight);
 
             //в) используя вывод со знаком $.
-            Console.WriteLine("{0} {1}, возраст: {2}, рост: {3:C2}, вес: {4:C2}", name, surname, age, growth, weight);
+            // Console.WriteLine("{0} {1}, возраст: {2}, рост: {3:C2}, вес: {4:C2}", name, surname, age, growth, weight);
+
+            /*2.	Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле /k’; 
+             * где m — масса тела в килограммах, h — рост в метрах.*/
+            growth = GetNumber("Введите Ваш рост для расчёта индекса массы:");
+            weight = GetNumber("Введите Ваш вес для расчёта индекса массы:");
+            Console.WriteLine("Ваш индекс массы тела равен: {0:F2}", BodyMassIndex(growth, weight));
 
 
             Console.ReadLine();
