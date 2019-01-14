fdb9c75eaeae07bf49a0ac347447ce246d85f5ee
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-14 17:58:45 +0300

    Задача 1.Написать программу сложения двух чисел.

--- C_Sharp_Level_1/C_Sharp_Level_1/Program.cs
+++ C_Sharp_Level_1/C_Sharp_Level_1/Program.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 #region 
-/*Котельников Андрей курс C# Уровень 1
+/*Котельников Андрей 
+ *Kурс C# Уровень 1
  *Урок 1
  *Практическая часть */
 #endregion
@@ -15,8 +16,42 @@ namespace C_Sharp_Level_1
 {
     class Program
     {
+        static double EnterNumber(string numberOfNumber)
+        {
+            string answer;
+            double number;
+
+            while (true)
+            {
+                Console.WriteLine("Введите " + numberOfNumber + " число:");
+                answer = Console.ReadLine();
+                if (Double.TryParse(answer, out number))
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Требуется ввести число. Попробуйте ещё раз\n");
+                }
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            //Задача 1.Написать программу сложения двух чисел.
+            double firstNumber;
+            double secondNamber;
+            double summa;
+
+            firstNumber = EnterNumber("первое");
+            secondNamber = EnterNumber("второе");
+
+            summa = firstNumber + secondNamber;
+
+            Console.WriteLine("\n" + firstNumber + " + " + secondNamber + " = " + summa);
+
+            Console.ReadLine();
         }
     }
 }
