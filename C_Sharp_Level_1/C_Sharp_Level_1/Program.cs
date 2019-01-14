1781d67a6ee364ea01cce90b90ac3849ac91be77
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-14 17:26:47 +0300

    Create new project.

---
+++ C_Sharp_Level_1/C_Sharp_Level_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region 
+/*Котельников Андрей курс C# Уровень 1
+ *Урок 1
+ *Практическая часть */
+#endregion
+
+
+namespace C_Sharp_Level_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
