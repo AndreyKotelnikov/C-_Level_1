7504edd9d8286433d86da9420c6d63a640973da2
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-20 17:48:46 +0300

    Create new project Lesson2_HomeWork.

---
+++ C_Sharp_Level_1/Lesson2_HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson1_HomeWork;
+
+#region
+//Котельников Андрей 
+//Kурс C# Уровень 1
+//Урок 2
+//Домашнее задание
+#endregion
+
+namespace Lesson2_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
