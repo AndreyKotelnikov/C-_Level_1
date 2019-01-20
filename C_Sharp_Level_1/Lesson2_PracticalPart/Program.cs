cf4543779953f13e8a5c01b8671e96e0d4e46dff
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-20 11:59:01 +0300

    Найти сумму цифр числа A. Рекурсией и без.

--- C_Sharp_Level_1/Lesson2_PracticalPart/Program.cs
+++ C_Sharp_Level_1/Lesson2_PracticalPart/Program.cs
@@ -24,7 +24,29 @@ namespace Lesson2_PracticalPart
             //Цикл с помощью рекурсии
             Loop(3, 13);
 
+            //Найти сумму цифр числа A
+            int a = (int) UsefulMethods.GetNumberFromConsoleInput("Введи число для суммы цифр");
+            Console.WriteLine("Сумма цифр числа подсчитана без рекурсии: {0}", SumDigitsOfNumber(a));
+            Console.WriteLine("Сумма цифр числа подсчитана с рекурсией: {0}", SumDigitsOfNumberRecursion(a));
+
             UsefulMethods.Pause();
+        }
+
+        private static int SumDigitsOfNumberRecursion(int a)
+        {
+            if (a < 1) return 0;
+            return SumDigitsOfNumberRecursion(a / 10) + (a % 10);
+        }
+
+        private static int SumDigitsOfNumber(int a)
+        {
+            int sum = 0;
+            while (a > 0)
+            {
+                sum += (a % 10);
+                a /= 10;
+            }
+            return sum;
         }
 
         private static void Loop(int a, int b)
