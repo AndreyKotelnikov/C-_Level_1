599905d3e6b50b456ea2b2f6194d0009120de63d
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-20 11:27:27 +0300

    Подключил класс UsefulMethods и сделал цикл с помощью рекурсии

--- C_Sharp_Level_1/Lesson2_PracticalPart/Program.cs
+++ C_Sharp_Level_1/Lesson2_PracticalPart/Program.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using Lesson1_HomeWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region 
+//Котельников Андрей 
+//Kурс C# Уровень 1
+//Урок 2
+//Практическая часть
+#endregion
+
+
 namespace Lesson2_PracticalPart
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            //Цикл с помощью рекурсии
+            Loop(3, 13);
+
+            UsefulMethods.Pause();
+        }
+
+        private static void Loop(int a, int b)
+        {
+            Console.Write("{0,4} ", a);
+            if (a < b)  Loop(a + 1, b);
         }
     }
 }
