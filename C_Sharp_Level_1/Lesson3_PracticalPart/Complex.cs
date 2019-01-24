7b6057ac2ed68330e966791238c088a6c0047b2c
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-24 21:51:23 +0300

    Create class Complex.

---
+++ C_Sharp_Level_1/Lesson3_PracticalPart/Complex.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_PracticalPart
+{
+    class Complex
+    {
+        double re;
+        double im;
+
+        double Re
+        {
+            get { return re; }
+            set { if (value < 0) Console.WriteLine("Создаём отрицательную вещественную часть"); }
+        }
+
+        double Im
+        {
+            get { return im; }
+            set { if (value < 0) Console.WriteLine("Создаём отрицательную мнимую часть"); }
+        }
+
+        public Complex()
+        {
+            re = 0;
+            im = 0;
+        }
+
+        public Complex(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+        public Complex Plus(Complex complexIn)
+        {
+            Complex complexOut = new Complex(complexIn.Re + Re, complexIn.Im + Im);
+            return complexOut;
+        }
+
+        public static Complex operator +(Complex complex1, Complex complex2)
+        {
+            Complex complexOut = new Complex(complex1.Re + complex2.Re, complex1.Im + complex2.Im);
+            return complexOut;
+        }
+
+        public override string ToString()
+        {
+            return $"({Re}, {Im})";
+        }
+    }
+}
