65956355526d69f1e25f6addb7c4340d9ddeed97
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-24 21:25:22 +0300

    Create struct ComplexStruct.

---
+++ C_Sharp_Level_1/Lesson3_PracticalPart/ComplexStruct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_PracticalPart
+{
+    struct ComplexStruct
+    {
+        readonly double re;
+        readonly double im;
+
+        public double Re => re;
+        public double Im => im;
+
+        public ComplexStruct(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+        public ComplexStruct Plus(ComplexStruct complexStructIn)
+        {
+            ComplexStruct complexStructOut = new ComplexStruct(complexStructIn.Re + this.Re, complexStructIn.Im + this.Im);
+            return complexStructOut;
+        }
+
+        public static ComplexStruct operator +(ComplexStruct complex1, ComplexStruct complex2)
+        {
+            ComplexStruct complexStructOut = new ComplexStruct(complex1.Re + complex2.Re, complex1.Im + complex2.Im);
+            return complexStructOut;
+        }
+
+        public override string ToString()
+        {
+            return $"({this.Re}, {this.Im})";
+        }
+    }
+}
--- C_Sharp_Level_1/Lesson3_PracticalPart/Program.cs
+++ C_Sharp_Level_1/Lesson3_PracticalPart/Program.cs
@@ -11,6 +11,22 @@ namespace Lesson3_PracticalPart
     {
         static void Main(string[] args)
         {
+            ComplexStruct complex1 = new ComplexStruct(4, 4);
+            ComplexStruct complex2 = new ComplexStruct(7, 7);
+
+            Console.WriteLine(complex1.ToString());
+            Console.WriteLine(complex2.ToString());
+
+            ComplexStruct complex3 = new ComplexStruct();
+            complex3 = complex1.Plus(complex2);
+
+            Console.WriteLine(complex3.ToString());
+
+            ComplexStruct complex4 = new ComplexStruct();
+            complex4 = complex1 + complex2;
+
+            Console.WriteLine(complex4.ToString());
+
             Pause();
         }
     }
