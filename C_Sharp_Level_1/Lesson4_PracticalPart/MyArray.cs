e9c4b58334d8edaa3ca1e5338cdaed3325cebbfb
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-01-28 09:40:18 +0300

    Задача 1. Класс «Мой одномерный массив». Разработать класс для работы с одномерным массивом.

---
+++ C_Sharp_Level_1/Lesson4_PracticalPart/MyArray.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_PracticalPart
+{
+    class MyArray
+    {
+        int[] array;
+
+        public MyArray(int size, int itemValue = 0)
+        {
+            array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = itemValue;
+            }
+        }
+
+        public MyArray(int size, int minValue, int maxValue)
+        {
+            array = new int[size];
+            Random rand = new Random(100);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(minValue, maxValue + 1);
+            }
+        }
+
+        public double Average
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];
+                }
+                return (double)sum / array.Length; 
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                int max = array[0];
+                for (int i = 1; i < array.Length; i++)
+                {
+                    if (max < array[i]) max = array[i];
+                }
+                return max;
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                int min = array[0];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (min > array[i]) min = array[i];
+                }
+                return min;
+            }
+        }
+        
+        public int Positive
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > 0) count++;
+                }
+                return count;
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                str += $"{array[i]}, ";
+            }
+            return str;
+        }
+    }
+}
