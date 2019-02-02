612ac27aaa5887efdce12ebc02468c1770d66ec9
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-02-02 21:03:53 +0300

    Сравниваем производительность работы StringBuilder с неизменяемыми строками

--- C_Sharp_Level_1/Lesson5_PracticalPart/Program.cs
+++ C_Sharp_Level_1/Lesson5_PracticalPart/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Lesson1_HomeWork.UsefulMethods;
+
 
 namespace Lesson5_PracticalPart
 {
     class Program
     {
+        const int iIterations = 10000;
+
         static void Main(string[] args)
         {
+            //Сравним производительность работы StringBuilder с неизменяемыми строками:
+            {
+                DateTime dt = DateTime.Now;
+                string str = String.Empty;
+                for (int i = 0; i < iIterations; i++)
+                    str += "abcdefghijklmnopqrstuvwxyz\r\n";
+                Console.WriteLine(DateTime.Now - dt);
+            }
+
+            //Те же операции с StringBuilder:
+            {
+                DateTime dt = DateTime.Now;
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < iIterations; i++)
+                    sb.Append("abcdefghijklmnopqrstuvwxyz\r\n");
+                string str = sb.ToString();
+                Console.WriteLine(DateTime.Now - dt);
+            }
+            //StringBuilder в 1000 раз быстрее string
+
+            Pause();
         }
     }
 }
