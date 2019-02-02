eaf295855dd5041b77f62b7529bd845cbbe716c0
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-02-02 21:11:35 +0300

    Create new project Lesson5_HomeWork.

---
+++ C_Sharp_Level_1/Lesson5_HomeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lesson1_HomeWork.UsefulMethods;
+using Lesson4_HomeWork;
+
+
+namespace Lesson5_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.	Создать программу, которая будет проверять корректность ввода логина. 
+            //Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита 
+            //или цифры, при этом цифра не может быть первой:
+            //а) без использования регулярных выражений;   AccountArray
+
+        }
+    }
+}
