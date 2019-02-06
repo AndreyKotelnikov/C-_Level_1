be54b0b11201e42960dabdfc30e09754e83db496
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-02-06 17:54:34 +0300

    1.	Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).

--- C_Sharp_Level_1/Lesson6_HomeWork/Program.cs
+++ C_Sharp_Level_1/Lesson6_HomeWork/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Lesson1_HomeWork.UsefulMethods;
+using static Lesson6_HomeWork.TableFunc;
 
 namespace Lesson6_HomeWork
 {
@@ -11,6 +12,17 @@ namespace Lesson6_HomeWork
     {
         static void Main(string[] args)
         {
+            //1. Изменить программу вывода таблицы функции так, 
+            //чтобы можно было передавать функции типа double (double, double). 
+            //Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+            Fun fun = Func1;
+            Table(fun, 4, 5);
+            Table(Func2, 4, 5);
+            Table(delegate (double x, double b) { return x * x * b; }, 4, 5);
+
+
+
+
 
 
             Pause();
---
+++ C_Sharp_Level_1/Lesson6_HomeWork/TableFunc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_HomeWork
+{
+    public delegate double Fun(double x, double b);
+
+    class TableFunc
+    {
+        
+        public static void Table(Fun F, double x, double b)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, b));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+        /// <summary>
+        /// a*x^2
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="a"></param>
+        /// <returns>a*x^2</returns>
+        public static double Func1(double x, double a) => a * Math.Pow(x, 2);
+
+        /// <summary>
+        /// a*sin(x)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="a"></param>
+        /// <returns>a*sin(x)</returns>
+        public static double Func2(double x, double a) => a * Math.Sin(x);
+
+    }
+}
