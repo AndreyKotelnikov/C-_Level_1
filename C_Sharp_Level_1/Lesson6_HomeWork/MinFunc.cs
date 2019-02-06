7823a841edcb9c357690c741a7a7b36418be4d40
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-02-06 20:02:55 +0300

    2.	Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата. а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить минимум. Использовать массив (или список) делегатов, в котором хранятся различные функции.

---
+++ C_Sharp_Level_1/Lesson6_HomeWork/MinFunc.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_HomeWork
+{
+    delegate double del_d_d(double d);
+
+    class MinFunc
+    {
+        
+        public static Dictionary<del_d_d, string> functions = new Dictionary<del_d_d, string>
+        {
+            [F] = "x * x - 50 * x + 10",
+            [Math.Sin] = "Sin(x)",
+            [Math.Tan] = "Tangent(x)"
+        };
+            
+        public static double[] GetUserAnswer()
+        {
+            double[] inputValues;
+
+            Console.WriteLine("\nСначала укажите номер функции, потом через пробелы: начальное значение функции, " +
+                "конечное значение и шаг увеличения аргумента:");
+            for (int i = 0; i < functions.Count; i++)
+            {
+                Console.WriteLine($"№{i + 1}: {functions.Values.ElementAt(i)} ");
+            }
+
+            while (true)
+            {
+                if (CheckOutput(out inputValues, functions.Count)) break;
+                Console.WriteLine("\nНеверный формат входящих даный. Попробуйте ещё:");
+            }
+            return inputValues;
+        }
+
+        private static bool CheckOutput(out double[] inputValues, int maxValue)
+        {
+            try
+            {
+                int size = 4;
+                inputValues = new double[size];
+                string[] str = Console.ReadLine().Split(' ');
+                for (int i = 0; i < inputValues.Length; i++)
+                {
+                    if (!double.TryParse(str[i], out inputValues[i])) return false;
+                    if (i == 0 && (inputValues[i] < 1 || inputValues[i] > maxValue)) return false;
+                    else inputValues[i]--;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                inputValues = (double[])Array.CreateInstance(typeof(double), 0);
+                return false;
+            }
+        }
+
+        public static double F(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+
+
+        public static void SaveFunc(string fileName, Func<double, double> result, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(result(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+    }
+}
--- C_Sharp_Level_1/Lesson6_HomeWork/Program.cs
+++ C_Sharp_Level_1/Lesson6_HomeWork/Program.cs
@@ -15,11 +15,23 @@ namespace Lesson6_HomeWork
             //1. Изменить программу вывода таблицы функции так, 
             //чтобы можно было передавать функции типа double (double, double). 
             //Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+            Console.WriteLine("Таблица функций:");
             Fun fun = Func1;
             Table(fun, 4, 5);
             Table(Func2, 4, 5);
             Table(delegate (double x, double b) { return x * x * b; }, 4, 5);
 
+            //2. Модифицировать программу нахождения минимума функции так, 
+            //чтобы можно было передавать функцию в виде делегата. 
+            //а) Сделать меню с различными функциями и представить пользователю выбор, 
+            //для какой функции и на каком отрезке находить минимум.
+            //Использовать массив(или список) делегатов, в котором хранятся различные функции.
+            Console.WriteLine("\n\nНахождение минимума функции:");
+            double[] userAnswes = MinFunc.GetUserAnswer();
+            del_d_d del = MinFunc.functions.Keys.ElementAt((int)userAnswes[0]);
+            Func<double, double> func = new Func<double, double>(del);
+            MinFunc.SaveFunc(@"..\..\data.bin", func, -100, 100, 0.5);
+            Console.WriteLine($"\nМинимум функции: {MinFunc.Load(@"..\..\data.bin"):0.000}");
 
 
 
