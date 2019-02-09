67ea83045be3d306e36dc97eb96de9b513498f58
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-02-09 18:31:04 +0300

    Добавил всплывающее сообщение: Привет {текст с формы}.

--- C_Sharp_Level_1/Lesson7_PracticalPart/Program.cs
+++ C_Sharp_Level_1/Lesson7_PracticalPart/Program.cs
@@ -9,9 +9,26 @@ namespace Lesson7_PracticalPart
 {
     class Program
     {
+        static void BtnClick(object o, EventArgs e)
+        {
+            MessageBox.Show($"Привет, {text.Text}!");
+        }
+
+        static TextBox text = new TextBox();
+
         static void Main(string[] args)
         {
             Form f = new Form();
+            Button button = new Button();
+            button.Left = 50;
+            button.Top = 50;
+            button.Text = "Жми меня";
+            button.Click += BtnClick;
+            f.Controls.Add(button);
+            f.Controls.Add(text);
+
+            f.Show();
+            Application.Run(f);
             
         }
     }
