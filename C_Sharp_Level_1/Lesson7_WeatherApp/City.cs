08a8b6d6adf51eb85849ae8443848ac8bc0b4044
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-02-10 18:39:14 +0300

    Complite project Lesson7_WeatherApp.

---
+++ C_Sharp_Level_1/Lesson7_WeatherApp/City.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_WeatherApp
+{
+    class City
+    {
+        public string Name { get; set; }
+        public int TemperatureMin { get; set; }
+        public int TemperatureMax { get; set; }
+        public int PressureMin { get; set; }
+        public int PressureMax { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        internal string Print()
+        {
+            return $"{Name, 10} :{TemperatureMin, 3} -{TemperatureMax, 3} C°, {PressureMin, 4} -{PressureMax, 4} мм рт. ст.";
+        }
+    }
+}
--- C_Sharp_Level_1/Lesson7_WeatherApp/Form1.cs
+++ C_Sharp_Level_1/Lesson7_WeatherApp/Form1.cs
@@ -10,16 +10,36 @@ using System.Windows.Forms;
 
 namespace Lesson7_WeatherApp
 {
-    public partial class Form1 : Form
+    public partial class FormWeatheApp : Form
     {
-        public Form1()
+
+        Repository data;
+
+        public FormWeatheApp()
         {
             InitializeComponent();
+
+            data = new Repository();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void FormWeatheApp_Load(object sender, EventArgs e)
         {
+            foreach (var item in data.Cities)
+            {
+                lbSites.Items.Add(item);
+            }
+        }
 
+        private void lbSites_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbSites.SelectedIndex == -1) { return; }
+
+            City currentCity = (City)lbSites.SelectedItem;
+
+            lbTemperatureValueMim.Text = $"{currentCity.TemperatureMin} C°";
+            lbTemperatureValueMax.Text = $"{currentCity.TemperatureMax} C°";
+            lbPressureValueMin.Text = $"{currentCity.PressureMin} мм рт. ст.";
+            lbPressureValueMax.Text = $"{currentCity.PressureMax} мм рт. ст.";
         }
     }
 }
