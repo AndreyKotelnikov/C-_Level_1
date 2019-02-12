7a5abab05e6b3001a9a148f3178c8d38c2c6661c
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-02-12 20:14:35 +0300

    Create new project 8.3_Lesson_8_3_HomeWork.

---
+++ C_Sharp_Level_1/8.3_Lesson_8_3_HomeWork/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace _8._3_Lesson_8_3_HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.miOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.miSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.miExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tboxQuestion = new System.Windows.Forms.TextBox();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.cboxTrue = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSaveQuest = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(9, 9);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(45, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miNew,
+            this.miOpen,
+            this.miSave,
+            this.miExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // miNew
+            // 
+            this.miNew.Name = "miNew";
+            this.miNew.Size = new System.Drawing.Size(180, 22);
+            this.miNew.Text = "New";
+            this.miNew.Click += new System.EventHandler(this.miNew_Click);
+            // 
+            // miOpen
+            // 
+            this.miOpen.Name = "miOpen";
+            this.miOpen.Size = new System.Drawing.Size(180, 22);
+            this.miOpen.Text = "Open";
+            this.miOpen.Click += new System.EventHandler(this.miOpen_Click);
+            // 
+            // miSave
+            // 
+            this.miSave.Name = "miSave";
+            this.miSave.Size = new System.Drawing.Size(180, 22);
+            this.miSave.Text = "Save";
+            this.miSave.Click += new System.EventHandler(this.miSave_Click);
+            // 
+            // miExit
+            // 
+            this.miExit.Name = "miExit";
+            this.miExit.Size = new System.Drawing.Size(180, 22);
+            this.miExit.Text = "Exit";
+            this.miExit.Click += new System.EventHandler(this.miExit_Click);
+            // 
+            // tboxQuestion
+            // 
+            this.tboxQuestion.Location = new System.Drawing.Point(13, 37);
+            this.tboxQuestion.Multiline = true;
+            this.tboxQuestion.Name = "tboxQuestion";
+            this.tboxQuestion.Size = new System.Drawing.Size(788, 160);
+            this.tboxQuestion.TabIndex = 1;
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(15, 27);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(67, 29);
+            this.nudNumber.TabIndex = 2;
+            this.nudNumber.Click += new System.EventHandler(this.nudNumber_ValueChanged);
+            // 
+            // cboxTrue
+            // 
+            this.cboxTrue.AutoSize = true;
+            this.cboxTrue.Location = new System.Drawing.Point(113, 28);
+            this.cboxTrue.Name = "cboxTrue";
+            this.cboxTrue.Size = new System.Drawing.Size(96, 28);
+            this.cboxTrue.TabIndex = 3;
+            this.cboxTrue.Text = "Правда";
+            this.cboxTrue.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(14, 226);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(108, 35);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(128, 226);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(108, 35);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSaveQuest
+            // 
+            this.btnSaveQuest.Location = new System.Drawing.Point(229, 20);
+            this.btnSaveQuest.Name = "btnSaveQuest";
+            this.btnSaveQuest.Size = new System.Drawing.Size(116, 42);
+            this.btnSaveQuest.TabIndex = 6;
+            this.btnSaveQuest.Text = "Сохранить";
+            this.btnSaveQuest.UseVisualStyleBackColor = true;
+            this.btnSaveQuest.Click += new System.EventHandler(this.btnSaveQuest_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboxTrue);
+            this.groupBox1.Controls.Add(this.btnSaveQuest);
+            this.groupBox1.Controls.Add(this.nudNumber);
+            this.groupBox1.Location = new System.Drawing.Point(442, 202);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 65);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 279);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tboxQuestion);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Верю - Неверю: Редактор вопросов";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miNew;
+        private System.Windows.Forms.ToolStripMenuItem miOpen;
+        private System.Windows.Forms.ToolStripMenuItem miSave;
+        private System.Windows.Forms.ToolStripMenuItem miExit;
+        private System.Windows.Forms.TextBox tboxQuestion;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.CheckBox cboxTrue;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSaveQuest;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ C_Sharp_Level_1/8.3_Lesson_8_3_HomeWork/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8._3_Lesson_8_3_HomeWork
+{
+
+    //Андрей Котельников
+    //3.	а) Создать приложение, показанное на уроке, добавив в него защиту от возможных ошибок 
+    //(не создана база данных, обращение к несуществующему вопросу, открытие слишком большого файла и т.д.).
+    //б) Изменить интерфейс программы, увеличив шрифт, поменяв цвет элементов 
+    //и добавив другие «косметические» улучшения на свое усмотрение.
+    //в) Добавить в приложение меню «О программе» с информацией о программе(автор, версия, авторские права и др.).
+    //г)* Добавить пункт меню Save As, в котором можно выбрать имя для сохранения базы данных(элемент SaveFileDialog).
+
+    public partial class Form1 : Form
+    {
+        // База данных с вопросами
+        TrueFalse database;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Обработчик пункта меню Exit
+        private void miExit_Click(object sender, EventArgs e)
+        {
+            if (database.IsChanged)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Есть несохранённые изменения.\nСохранить изменения в файл перед выходом?",
+                    "Сохранить изменения перед выходом?", MessageBoxButtons.YesNoCancel);
+                if (result == DialogResult.Yes) { miSave_Click(null, null); }
+                else if (result == DialogResult.Cancel) { return; }
+            }
+            Close();
+        }
+        // Обработчик пункта меню New
+        private void miNew_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(sfd.FileName);
+                database.Add(string.Empty, false);
+                database.Save();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = 1;
+                nudNumber.Value = 1;
+            };
+        }
+        // Обработчик события изменения значения numericUpDown
+        private void nudNumber_ValueChanged(object sender, EventArgs e)
+        {
+            if (database == null) { return; }
+            if (!CheckSaveCurrentQuestion())
+            {
+                if (DialogResult.Yes != MessageBox.Show(
+                    "Для перехода на другой вопрос, нужно сохранить или удалить текущий.\nСохранить этот вопрос?",
+                    "Сохранить текущий вопрос?", MessageBoxButtons.YesNo)) { return; }
+                else { btnSaveQuest_Click(null, null); }
+            }
+            if (!CheckChangesInCurrentQuestion())
+            {
+                if (DialogResult.Yes == MessageBox.Show(
+                    "Текущий вопрос был изменён.\nСохранить изменения?",
+                    "Сохранить изменения?", MessageBoxButtons.YesNo)) { btnSaveQuest_Click(null, null); }
+            }
+            tboxQuestion.Text = database[(int)nudNumber.Value - 1].text;
+            cboxTrue.Checked = database[(int)nudNumber.Value - 1].trueFalse;
+        }
+        // Обработчик кнопки Добавить
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Создайте новую базу данных", "Сообщение");
+                return;
+            }
+            if (!CheckSaveCurrentQuestion())
+            {
+                if (DialogResult.Yes != MessageBox.Show(
+                    "Для создания нового вопроса нужно сохранить текущий.\nСохранить этот вопрос?",
+                    "Сохранить текущий вопрос?", MessageBoxButtons.YesNo)) { return; }
+                else { btnSaveQuest_Click(null, null); }
+            }
+            if (!CheckChangesInCurrentQuestion())
+            {
+                if (DialogResult.Yes == MessageBox.Show(
+                    "Текущий вопрос был изменён.\nСохранить изменения?",
+                    "Сохранить изменения?", MessageBoxButtons.YesNo)) { btnSaveQuest_Click(null, null); }
+            }
+            database.Add(string.Empty, false);
+            nudNumber.Maximum = database.Count;
+            nudNumber.Value = database.Count;
+            tboxQuestion.Text = string.Empty;
+            cboxTrue.Checked = false;
+        }
+        // Обработчик кнопки Удалить
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (nudNumber.Maximum == 1 || database == null) return;
+            database.Remove((int)nudNumber.Value);
+            nudNumber.Maximum--;
+            if (nudNumber.Value > 1) nudNumber.Value = nudNumber.Value;
+        }
+        // Обработчик пункта меню Save
+        private void miSave_Click(object sender, EventArgs e)
+        {
+            if (database != null)
+            {
+                database.Save();
+                database.IsChanged = false;
+            }
+            else MessageBox.Show("База данных не создана");
+        }
+        // Обработчик пункта меню Open
+        private void miOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(ofd.FileName);
+                database.Load();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = database.Count;
+                nudNumber.Value = 1;
+                tboxQuestion.Text = database[(int)nudNumber.Value - 1].text;
+                cboxTrue.Checked = database[(int)nudNumber.Value - 1].trueFalse;
+            }
+        }
+        // Обработчик кнопки Сохранить (вопрос)
+        private void btnSaveQuest_Click(object sender, EventArgs e)
+        {
+            if (database == null) { MessageBox.Show("База данных не создана"); return; }
+            if (!CheckChangesInCurrentQuestion())
+            {
+                database[(int)nudNumber.Value - 1].text = tboxQuestion.Text;
+                database[(int)nudNumber.Value - 1].trueFalse = cboxTrue.Checked;
+                database.IsChanged = true;
+            }
+        }
+
+        //Проверка на сохранение нового вопроса в database
+        private bool CheckSaveCurrentQuestion()
+        {
+            return (database[(int)nudNumber.Value - 1].text == string.Empty) ? false : true; 
+        }
+
+        //Проверка на изменение текста вопроса и признака trueFalse 
+        private bool CheckChangesInCurrentQuestion()
+        {
+            return (database[(int)nudNumber.Value - 1].text != tboxQuestion.Text ||
+                database[(int)nudNumber.Value - 1].trueFalse != cboxTrue.Checked) ? false : true;
+        }
+    }
+}
